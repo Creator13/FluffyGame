cfb6758498759b35ef9eeec24db8b7bba56ba00e
Casper van Battum <cvanbattum@gmail.com>
2021-04-02 17:36:18 +0200

    Added camera bounding script

---
+++ Fluffy/Assets/Scripts/CameraBounds2D.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace CameraBounding
+{
+    public class CameraBounds2D : MonoBehaviour
+    {
+        public Vector2 scaleBound = new Vector2(1, 1);
+        public Vector2 offset;
+
+        [HideInInspector] public Vector2 maxXlimit;
+        [HideInInspector] public Vector2 maxYlimit;
+
+        Camera _camera;
+
+        private void Start()
+        {
+            var sprite = GetComponent<SpriteRenderer>();
+            if (sprite)
+            {
+                // scaleBound = sprite.bounds.extents * 2;
+            }
+        }
+
+        public void Initialize(Camera camera)
+        {
+            _camera = camera;
+            CalculateBounds();
+        }
+
+        private void Update()
+        {
+            CalculateBounds();
+        }
+
+        private void CalculateBounds()
+        {
+            var cameraHalfWidth = _camera.aspect * _camera.orthographicSize;
+            maxXlimit = new Vector2((transform.position.x + offset.x - (scaleBound.x / 2)) + cameraHalfWidth,
+                (transform.position.x + offset.x + (scaleBound.x / 2)) - cameraHalfWidth);
+            maxYlimit = new Vector2((transform.position.y + offset.y - (scaleBound.y / 2)) + _camera.orthographicSize,
+                (transform.position.y + offset.y + (scaleBound.y / 2)) - _camera.orthographicSize);
+        }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.color= Color.red;
+            Gizmos.DrawWireCube(transform.position + new Vector3(offset.x, offset.y, 0), scaleBound);
+            // Gizmos.DrawWireCube();
+        }
+    }
+}
--- Fluffy/Assets/Scripts/FollowPlayer2D.cs
+++ Fluffy/Assets/Scripts/FollowPlayer2D.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using CameraBounding;
 using UnityEngine;
 
 namespace Fluffy
 {
+    [RequireComponent(typeof(Camera))]
     public class FollowPlayer2D : MonoBehaviour
     {
         [SerializeField] private Transform target;
@@ -12,6 +11,9 @@ namespace Fluffy
         [SerializeField] private float zOffsetOverride;
         [SerializeField] private Vector2 XYOffset;
 
+        [SerializeField] private CameraBounds2D bounds;
+
+        //TODO This could potentially be made more efficient by caching this value. It's unlikely to change while playing in editor and impossible to change in prod
         private float ZOffset => Mathf.Abs(zOffsetOverride) <= 0.001 ? transform.position.z : zOffsetOverride;
 
         private Vector3 TargetPos
@@ -22,8 +24,17 @@ namespace Fluffy
                 targetPos.z = ZOffset;
                 targetPos.x += XYOffset.x;
                 targetPos.y += XYOffset.y;
+
+                targetPos.x = Mathf.Clamp(targetPos.x, bounds.maxXlimit.x, bounds.maxXlimit.y);
+                targetPos.y = Mathf.Clamp(targetPos.y, bounds.maxYlimit.x, bounds.maxYlimit.y);
+
                 return targetPos;
             }
+        }
+
+        private void Awake()
+        {
+            bounds.Initialize(GetComponent<Camera>());
         }
 
         private void Start()
@@ -35,8 +46,8 @@ namespace Fluffy
         {
             var targetPos = TargetPos;
 
-            if (Vector3.Distance(transform.position, targetPos) < 0.05) return;
-            
+            if (Vector3.Distance(transform.position, targetPos) < 0.001) return;
+
             transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
         }
     }
