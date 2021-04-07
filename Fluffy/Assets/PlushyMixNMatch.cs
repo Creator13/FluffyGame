ac684216e31baaca38a4c251a2efe7783c14945f
Casper van Battum <cvanbattum@gmail.com>
2021-04-07 19:11:24 +0200

    Customize your plushy! 🧸

---
+++ Fluffy/Assets/PlushyMixNMatch.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Fluffy
+{
+    [System.Serializable]
+    public class PlushyOption
+    {
+        public string name;
+        public Sprite sprite;
+    }
+
+    public class PlushyMixNMatch : MonoBehaviour
+    {
+        [SerializeField] private List<PlushyOption> baseColors = new List<PlushyOption>(3);
+        [SerializeField] private List<PlushyOption> accessories = new List<PlushyOption>(3);
+        [SerializeField] private List<PlushyOption> patterns = new List<PlushyOption>(3);
+
+        [SerializeField] private SpriteRenderer baseColorSpriteRenderer;
+        [SerializeField] private SpriteRenderer accessorySpriteRenderer;
+        [SerializeField] private SpriteRenderer patternSpriteRenderer;
+
+        [Range(0, 2)] public int baseColorIndex;
+        [Range(0, 2)] public int accessoryIndex;
+        [Range(0, 2)] public int patternIndex;
+
+        public void SetBaseColor(string optionName)
+        {
+            baseColorIndex = baseColors.FindIndex(opt => opt.name == optionName);
+            UpdateSprites();
+        }
+
+        public void SetAccessory(string optionName)
+        {
+            accessoryIndex = accessories.FindIndex(opt => opt.name == optionName);
+            UpdateSprites();
+        }
+
+        public void SetPattern(string optionName)
+        {
+            patternIndex = patterns.FindIndex(opt => opt.name == optionName);
+            UpdateSprites();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            UpdateSprites();
+        }
+#endif
+
+        private void UpdateSprites()
+        {
+            baseColorSpriteRenderer.sprite = baseColors[baseColorIndex].sprite;
+            accessorySpriteRenderer.sprite = accessories[accessoryIndex].sprite;
+            patternSpriteRenderer.sprite = patterns[patternIndex].sprite;
+        }
+    }
+}
